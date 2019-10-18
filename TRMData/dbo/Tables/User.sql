163e34898254ed6a812c0ae75640018b385630fc
OB <octbj77@gmail.com>
2019-10-18 11:28:53 -0400

    Register DB Tables
    The Tables for the basic register system.

---
+++ TRMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [Lastname] NVARCHAR(50) NOT NULL, 
+    [EmailAdress] NVARCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
