2b48f2ea7e3effd3158e179b483851c200409b93
OB <octbj77@gmail.com>
2019-12-20 23:43:07 -0500

    Update Cart Quantities
    Added Automapper and INPC to enable cart quantity changes to be reflected.

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
-﻿using Caliburn.Micro;
+﻿using AutoMapper;
+using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using TRMDesktopUI.Helpers;
+using TRMDesktopUI.Models;
 using TRMDesktopUI.ViewModels;
 using TRMDesktopUILibrary.Api;
 using TRMDesktopUILibrary.Helpers;
@@ -28,8 +30,23 @@ namespace TRMDesktopUI
             "PasswordChanged");
         }
 
+        private IMapper ConfigureAutomapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+               cfg.CreateMap<ProductModel, ProductDisplayModel>();
+               cfg.CreateMap<CartItemModel, CartItemDisplayModel>();
+            });
+
+            var output = config.CreateMapper();
+
+            return output;
+        }
+
         protected override void Configure()
         {
+            _container.Instance(ConfigureAutomapper());
+
             _container.Instance(_container)
             .PerRequest<IProductEndPoint, ProductEndPoint>()
             .PerRequest<ISaleEndPoint, SaleEndPoint>();
--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -1,13 +1,17 @@
-﻿using Caliburn.Micro;
+﻿using AutoMapper;
+using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TRMDesktopUI.Models;
 using TRMDesktopUILibrary.Api;
 using TRMDesktopUILibrary.Helpers;
 using TRMDesktopUILibrary.Models;
+
+
 
 namespace TRMDesktopUI.ViewModels
 {
@@ -16,11 +20,15 @@ namespace TRMDesktopUI.ViewModels
         IProductEndPoint _productEndPoint;
         ISaleEndPoint _saleEndpoint;
         IConfigHelper _configHelper;
-        public SalesViewModel(IProductEndPoint productEndPoint, IConfigHelper configHelper, ISaleEndPoint saleEndpoint)
+        IMapper _mapper;
+
+        public SalesViewModel(IProductEndPoint productEndPoint, IConfigHelper configHelper,
+            ISaleEndPoint saleEndpoint, IMapper mapper)
         {
             _productEndPoint = productEndPoint;
             _saleEndpoint = saleEndpoint;
             _configHelper = configHelper;
+            _mapper = mapper;
         }
 
         protected override async void OnViewLoaded(object view)
@@ -32,12 +40,13 @@ namespace TRMDesktopUI.ViewModels
         public async Task LoadProducts()
         {
             var productList = await _productEndPoint.GetAll();
-            Products = new BindingList<ProductModel>(productList);
+            var products = _mapper.Map<List<ProductDisplayModel>>(productList);
+            Products = new BindingList<ProductDisplayModel>(products);
         }
 
-        private BindingList<ProductModel> _products;
+        private BindingList<ProductDisplayModel> _products;
 
-        public BindingList<ProductModel> Products
+        public BindingList<ProductDisplayModel> Products
         {
             get { return _products; }
             set
@@ -47,9 +56,9 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        private ProductModel _selectedProduct;
+        private ProductDisplayModel _selectedProduct;
 
-        public ProductModel SelectedProduct
+        public ProductDisplayModel SelectedProduct
         {
             get { return _selectedProduct; }
             set
@@ -61,9 +70,9 @@ namespace TRMDesktopUI.ViewModels
         }
 
 
-        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+        private BindingList<CartItemDisplayModel> _cart = new BindingList<CartItemDisplayModel>();
 
-        public BindingList<CartItemModel> Cart
+        public BindingList<CartItemDisplayModel> Cart
         {
             get { return _cart; }
             set
@@ -161,18 +170,16 @@ namespace TRMDesktopUI.ViewModels
 
         public void AddToCart()
         {
-            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            CartItemDisplayModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
             if (existingItem != null)
             {
                 existingItem.QuantityInCart += ItemQuantity;
-                //// Hack - There should be a better way of refreshing the cart display
-                Cart.Remove(existingItem);
-                Cart.Add(existingItem);
+
             }
             else 
             {
-               CartItemModel item = new CartItemModel
+               CartItemDisplayModel item = new CartItemDisplayModel
                 {
                     Product = SelectedProduct,
                     QuantityInCart = ItemQuantity
