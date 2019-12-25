d599a8a6efa7784fffae330d9d85bf144a629077
OB <octbj77@gmail.com>
2019-12-25 09:17:58 -0500

    Add Transactions
    Add transactions to SQL Data Access and used them for the SaleData call.

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -56,22 +56,35 @@ namespace TRMDataManager.Library.DataAccess
 
             sale.Total = sale.SubTotal + sale.Tax;
 
-            // Save the sale model
-            SqlDataAccess sql = new SqlDataAccess();
-            sql.SaveData("dbo.spSale_Insert", sale, "TRMData");
-
-            // Get the ID from the sale model
-            sale.Id = sql.LoadData<int, dynamic>("spSale_Lookup", new { sale.CashierId, sale.SaleDate }, "TRMData").FirstOrDefault();
-
-            // Finish filling in the sale detail models
-            foreach(var item in details)
-            {
-                item.SaleId = sale.Id;
-                // Save the sale detail models
-                sql.SaveData("dbo.spSaleDetail_Insert", item, "TRMData");
-            }
-
             
+            using (SqlDataAccess sql = new SqlDataAccess())
+            {
+                try
+                {
+                    sql.StartTransaction("TRMData");
+
+                    // Save the sale model
+                    sql.SaveDataInTransaction("dbo.spSale_Insert", sale);
+
+                    // Get the ID from the sale model
+                    sale.Id = sql.LoadDataInTransaction<int, dynamic>("spSale_Lookup", new { sale.CashierId, sale.SaleDate }).FirstOrDefault();
+
+                    // Finish filling in the sale detail models
+                    foreach (var item in details)
+                    {
+                        item.SaleId = sale.Id;
+                        // Save the sale detail models
+                        sql.SaveDataInTransaction("dbo.spSaleDetail_Insert", item);
+                    }
+
+                    sql.Committransaction();
+                }
+                catch 
+                {
+                    sql.RollbackTransaction();
+                    throw;
+                }
+            }
         }
     }
 
--- TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace TRMDataManager.Library.Internal.DataAccess
 {
-    public class SqlDataAccess
+    public class SqlDataAccess: IDisposable
     {
          internal string GetConnectionString (string name)
         {
@@ -39,5 +39,68 @@ namespace TRMDataManager.Library.Internal.DataAccess
             }
         }
 
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+
+        public void StartTransaction (string connectionStringName) 
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+
+            _transaction = _connection.BeginTransaction();
+
+            isClosed = false;
+        }
+
+        public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+        {
+             List<T> rows = _connection.Query<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+
+                return rows;
+        }
+
+        public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+        {
+            _connection.Execute(storedProcedure, parameters, commandType: CommandType.StoredProcedure, transaction: _transaction);
+        }
+
+        private bool isClosed = false;
+
+        public void Committransaction()
+        {
+            _transaction?.Commit();
+            _connection?.Close();
+
+            isClosed = true;
+        }
+
+        public void RollbackTransaction()
+        {
+            _transaction?.Rollback();
+            _connection?.Close();
+
+            isClosed = true;
+        }
+
+        public void Dispose()
+        {
+            if(isClosed == false )
+            {
+                try
+                {
+                    Committransaction();
+                }
+                catch 
+                {
+                    // TODO - Log this issue
+                }
+            }
+
+            _transaction = null;
+            _connection = null;
+           
+        }
     }
 }
