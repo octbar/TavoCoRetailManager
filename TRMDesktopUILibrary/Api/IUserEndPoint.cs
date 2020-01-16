05824c759acd510f0351f7d667db7bc6ebb1c5b1
OB <octbj77@gmail.com>
2020-01-16 14:08:55 -0500

    Add User Roles Management
    Add User Roles Management changes

--- TRMDesktopUILibrary/Api/IUserEndPoint.cs
+++ TRMDesktopUILibrary/Api/IUserEndPoint.cs
@@ -7,5 +7,9 @@ namespace TRMDesktopUILibrary.Api
     public interface IUserEndPoint
     {
         Task<List<UserModel>> GetAll();
+        Task<Dictionary<string, string>> GetAllRoles();
+        Task AddUserToRole(string userId, string role);
+        Task RemoveUserFromRole(string userId, string role);
+
     }
 }
