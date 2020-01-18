d4a657f049c09988fadeb448ea6fe9d99084ee6d
OB <octbj77@gmail.com>
2020-01-18 14:06:20 -0500

    Added New API and Moved Over Functionality

---
+++ TRMApi/Controllers/SaleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SaleController : ControllerBase
+    {
+        [Authorize(Roles = "Cashier")]
+        public void Post(SaleModel sale)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier); 
+            SaleData data = new SaleData();
+
+            data.SaveSale(sale, userId);
+        }
+
+        [Authorize(Roles = "Admin,Manager")]
+        [Route("GetSalesreport")]
+        public List<SaleReportModel> GetSalesReport()
+        {
+            SaleData data = new SaleData();
+            return data.GetSalereport();
+        }
+    }
+}
